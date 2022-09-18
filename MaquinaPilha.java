d0b7644169df761d4045d30b37166d4a93a49339
rszimmer <rdszimmer@inf.ufpel.edu.br>
2022-09-18 15:33:02 -0300

    Adicionada uma explicacao sobre os exercicios e como encontra-los nos arquivos, assim como foram corrigidos os nomes dos arquivos do exercicio 4

--- MaquinaPilha.java
+++ MaquinaPilha.java
@@ -1,47 +1,22 @@
-import java.util.*;
 import java.io.*;
 
-public class MaquinaPilha {
-    Stack<String> pilha = new Stack();
+class MaquinaPilha {
+    public static void main(String[]args) throws Exception
+	{
+        BufferedReader arquivo = new BufferedReader(new FileReader(args[0]));
+        String programString = "";
+        char currchar;
+        int currchar1 = 0;
+        int resultado = 0;
 
-    public MaquinaPilha (String programa) {
-        String[] partes = programa.split("\n");
-        pilha = setPilha(partes);
-    }
-
-    public Stack<String> setPilha (String[] partes) {
-        int i;
-        Stack<String> aux = new Stack();
-        for(i = 0; i < partes.length; i++) {
-            if(partes[i].indexOf("PUSH") != -1) aux.push(partes[i].split("PUSH ")[1]);
-            else if(partes[i].indexOf("PRINT") != -1);
-            else aux.push(partes[i]);
+        while (currchar1 != -1) {
+            currchar1 =  arquivo.read();
+			currchar = (char) currchar1;
+            if(currchar1 != -1) programString += currchar;
         }
-        return aux;
-    }
+        MaquinaPilhaExec instrucoes = new MaquinaPilhaExec(programString);
+        resultado = instrucoes.execute();
 
-    public int execute () {
-        String atual = pilha.pop();
-        int val1 = 0, val2 = 0;
-        switch (atual){
-                case "MULT":
-                    val1 = execute();
-                    val2 = execute();
-                    return val2*val1;
-                case "SUM":
-                    val1 = execute();
-                    val2 = execute();
-                    return val2+val1;
-                case "SUB":
-                    val1 = execute();
-                    val2 = execute();
-                    return val2-val1;
-                case "DIV":
-                    val1 = execute();
-                    val2 = execute();
-                    return val2/val1;
-                default:
-                    return Integer.parseInt(atual);
-            }
-    } 
+        System.out.println("Valor Pilha: " + resultado);
+    }
 }
---
+++ MaquinaPilhaExec.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class MaquinaPilhaExec {
+    Stack<String> pilha = new Stack();
+
+    public MaquinaPilhaExec (String programa) {
+        String[] partes = programa.split("\n");
+        pilha = setPilha(partes);
+    }
+
+    public Stack<String> setPilha (String[] partes) {
+        int i;
+        Stack<String> aux = new Stack();
+        for(i = 0; i < partes.length; i++) {
+            if(partes[i].indexOf("PUSH") != -1) aux.push(partes[i].split("PUSH ")[1]);
+            else if(partes[i].indexOf("PRINT") != -1);
+            else aux.push(partes[i]);
+        }
+        return aux;
+    }
+
+    public int execute () {
+        String atual = pilha.pop();
+        int val1 = 0, val2 = 0;
+        switch (atual){
+                case "MULT":
+                    val1 = execute();
+                    val2 = execute();
+                    return val2*val1;
+                case "SUM":
+                    val1 = execute();
+                    val2 = execute();
+                    return val2+val1;
+                case "SUB":
+                    val1 = execute();
+                    val2 = execute();
+                    return val2-val1;
+                case "DIV":
+                    val1 = execute();
+                    val2 = execute();
+                    return val2/val1;
+                default:
+                    return Integer.parseInt(atual);
+            }
+    } 
+}
